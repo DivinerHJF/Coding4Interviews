85143edbce63eaa8ce0d97996cda9dcb8bc94c0d
郝建锋 <1246574158@qq.com>
2020-04-24 15:42:17 +0800

    Add 016-合并两个或k个有序链表

--- A1 - 剑指 offer/016-合并两个或k个有序链表/solution.py
+++ A1 - 剑指 offer/016-合并两个或k个有序链表/solution.py
@@ -3,30 +3,76 @@
 
 
 class ListNode(object):
-    def __init__(self, x):
-        self.val = x
+    def __init__(self, item):
+        self.val = item
         self.next = None
 
 
 class SingleLinkList(object):
     def __init__(self):
-        """初始化，头结点为空"""
+        """创建头结点"""
         self._head = None
 
-    def is_empty(self):
-        """判断链表是否为空"""
-        return self._head is None
-
     def append(self, item):
-        """尾部添加元素"""
+        """ 尾部添加元素 """
         node = ListNode(item)
-        # 先判断是否为空链表
-        if self.is_empty():
-            # 空链表，_head 指向新结点
+        if self._head is None:
             self._head = node
         else:
-            # 不是空链表，则找到尾部，将尾部next结点指向新结点
             cur = self._head
             while cur.next is not None:
                 cur = cur.next
             cur.next = node
+
+    def items(self, node):
+        """ 从节点 node 遍历链表 """
+        cur = node
+        while cur is not None:
+            yield cur.val
+            cur = cur.next
+
+    # 迭代法
+    def merge_two_lists_diedai(self, l1: ListNode, l2: ListNode) -> ListNode:
+        cur = dum = ListNode(0)
+        while (l1 and l2):
+            if l1.val < l2.val:
+                cur.next, l1 = l1, l1.next
+            else:
+                cur.next, l2 = l2, l2.next
+            cur = cur.next
+        cur.next = l1 if l1 else l2
+        return dum.next
+
+    # 递归法
+    def merge_two_lists_digui(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if not l1:
+            return l2
+        if not l2:
+            return l1
+        if l1.val < l2.val:
+            l1.next = self.merge_two_lists_digui(l1.next, l2)
+            return l1
+        l2.next = self.merge_two_lists_digui(l1, l2.next)
+        return l2
+
+
+def main():
+    # 创建链表
+    list1 = SingleLinkList()
+    list2 = SingleLinkList()
+    list3, list4 = [1, 3, 4, 5], [1, 2, 4]
+    for i in list3:
+        list1.append(i)
+    for i in list4:
+        list2.append(i)
+    # 迭代法
+    # cur = list1.merge_two_lists_diedai(list1._head, list2._head)
+    # 递归法
+    cur = list1.merge_two_lists_digui(list1._head, list2._head)
+    while cur is not None:
+        print(cur.val, end='\t')
+        cur = cur.next
+
+
+if __name__ == "__main__":
+    main()
--- A1 - 剑指 offer/056-删除链表中重复的结点/solution.py
+++ A1 - 剑指 offer/056-删除链表中重复的结点/solution.py
@@ -3,30 +3,30 @@
 
 
 class ListNode(object):
-    def __init__(self, x):
-        self.val = x
+    def __init__(self, item):
+        self.val = item
         self.next = None
 
 
 class SingleLinkList(object):
     def __init__(self):
-        """初始化，头结点为空"""
+        """创建头结点"""
         self._head = None
 
-    def is_empty(self):
-        """判断链表是否为空"""
-        return self._head is None
-
     def append(self, item):
-        """尾部添加元素"""
+        """ 尾部添加元素 """
         node = ListNode(item)
-        # 先判断是否为空链表
-        if self.is_empty():
-            # 空链表，_head 指向新结点
+        if self._head is None:
             self._head = node
         else:
-            # 不是空链表，则找到尾部，将尾部next结点指向新结点
             cur = self._head
             while cur.next is not None:
                 cur = cur.next
             cur.next = node
+
+    def items(self, node):
+        """ 从节点 node 遍历链表 """
+        cur = node
+        while cur is not None:
+            yield cur.val
+            cur = cur.next
